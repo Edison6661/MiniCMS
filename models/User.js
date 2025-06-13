9a3f17b28350fafb78f7193f5e887c08392fb106
Edison6661 <edisonhuk1717@gmail.com>
2025-06-13 23:34:56 +0300

    Create User.js

---
+++ models/User.js
@@ -0,0 +1,28 @@
+const { DataTypes } = require('sequelize');
+const db = require('../config/db');
+
+const User = db.define('User', {
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: true
+    }
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  role: {
+    type: DataTypes.STRING,
+    defaultValue: 'user', // або 'admin'
+  }
+});
+
+module.exports = User;
