fc1936705a801ddb753246d04bce3ffc8c31191c
Edison6661 <edisonhuk1717@gmail.com>
2025-06-13 23:34:59 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const db = require('./config/db');
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Тестовий маршрут
+app.get('/', (req, res) => {
+  res.send('MiniCMS API працює 🎉');
+});
+
+// Підключення до бази
+db.authenticate()
+  .then(() => console.log('✅ Підключено до БД'))
+  .catch(err => console.error('❌ Помилка БД:', err));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Сервер запущено на порту ${PORT}`));
+
+const authRoutes = require('./routes/auth');
+
+// Після app.use(express.json())
+app.use('/api/auth', authRoutes);
+
+const postRoutes = require('./routes/posts');
+app.use('/api/posts', postRoutes);
