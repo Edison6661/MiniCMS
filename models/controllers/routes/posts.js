88a7947b7afeb2619c53ad0be85457f6c075c0a2
Edison6661 <edisonhuk1717@gmail.com>
2025-06-14 11:05:02 +0300

    Update posts.js

--- models/controllers/routes/posts.js
+++ models/controllers/routes/posts.js
@@ -10,3 +10,14 @@ router.put('/:id', auth, postController.updatePost);
 router.delete('/:id', auth, postController.deletePost);
 
 module.exports = router;
+const upload = require('../middleware/uploadMiddleware');
+
+// POST з медіа
+router.post('/', auth, upload.single('media'), async (req, res, next) => {
+  try {
+    req.body.media = req.file ? `/uploads/${req.file.filename}` : null;
+    next();
+  } catch (err) {
+    res.status(500).json({ message: 'Помилка завантаження медіа', error: err.message });
+  }
+}, postController.createPost);
